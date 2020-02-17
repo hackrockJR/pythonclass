e54d1ee31e6b889f0b400cc25c46410ec502c4b3
hackrockJR <jfromero160@hotmail.com>
2020-02-17 15:23:48 -0600

    Lista

--- hola.py
+++ hola.py
@@ -1,5 +1,21 @@
-print("Hola")
-print("Bienvenido al curso de Python")
-print ("Cambio desde Kali en la empresa1")
-print ("Cambio desde Windows en la empresa1")
+#print("Hola")
+#print("Bienvenido al curso de Python")
+#print ("Cambio desde Kali en la empresa1")
+#print ("Cambio desde Windows en la empresa1")
+
+
+#Crear una lista con variables y mensaje a imprimir
+lista1= [0,1,2,3,4]
+
+print("Este es el resultado de la lista ", lista1[3])
+
+# Variable len permite enumerar de la lista o cadena de caracteres
+Resultado= len(lista1)
+
+print ("Total de la lista=", Resultado)
+
+
+
+
+
 
