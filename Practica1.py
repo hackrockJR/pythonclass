bd6293eab1abf148fbf75a648cc65851ced23503
hackrockJR <jfromero160@hotmail.com>
2020-02-17 10:48:15 -0600

    variable_replace

--- Practica1.py
+++ Practica1.py
@@ -5,4 +5,11 @@ tweet2= tweet.upper()
 
 print(tweet2)
 
+# lower() // metodo para pasar a minuscula 
+# capitalize() // metodo para poner la primera letra en mayuscula. 
+# count("m") // metodo para buscar en la cadena de texto la letra m
+
+tweet3= tweet2.replace("PRIMER","SEGUNDO")
+
+print(tweet3)
 
