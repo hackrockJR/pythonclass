e7fb5c427d659d3d429c80b12e0a57529f9e90af
hackrockJR <jfromero160@hotmail.com>
2020-02-18 09:42:20 -0600

    Clase3

---
+++ Clase3.py
@@ -0,0 +1,108 @@
+# Juan Romero
+
+#netstel list
+
+# Lista de listas
+
+likedby = [10,14,21]
+
+tweet = "Primer Mensaje"
+
+lista =[1, "agonzalez", tweet]
+
+lista.append(likedby)
+
+# Print1
+
+print (lista)
+
+Resultado= lista[3]
+
+# Print2
+
+print(Resultado)
+
+Resultado2 = lista [3][1]
+
+# Print3
+print(Resultado2)
+
+#Tuplas inmutables
+
+# A diferencia de las listas no se puede modificar sus valores
+
+autor = "Juan Romero"
+creditos = ("oferta",2016,autor)
+
+
+# Print4
+print (creditos)
+
+#Eliminar la tuplas
+
+#del creditos
+
+# Diccionario
+# Clave con valor
+
+
+tweet2 = {"Autor": "Juan", "Fecha": "2020-02-18", "Texto": "Mi primer Mensaje"}
+tweet3 = {"Autor": "Fernando", "Fecha": "2020-02-18", "Texto": "Mi Segundo Mensaje"}
+
+Resultado3= tweet2["Autor"]
+
+# Print5
+print ("Nombre del autor en el diccionario=", Resultado3)
+
+# Agregar los diccionarios a una lista
+
+lista_tweets = [tweet2, tweet3]
+
+#Print6
+
+print (lista_tweets)
+
+#Imprimir del diccionario 1 el autor
+
+Resultado4= lista_tweets[0]["Autor"]
+
+# Print7
+
+print (Resultado4)
+
+# Imprimir del diccionario 2 el autor 
+
+Resultado5 = lista_tweets[1]["Autor"]
+
+#Print8
+
+print (Resultado5)
+
+# RESULTADOS DE LOS 8 PRINT>
+
+# [1, 'agonzalez', 'Primer Mensaje', [10, 14, 21]]
+# [10, 14, 21]
+# 14
+# ('oferta', 2016, 'Juan Romero')
+# Nombre del autor en el diccionario= Juan
+# [{'Autor': 'Juan', 'Fecha': '2020-02-18', 'Texto': 'Mi primer Mensaje'}, {'Autor': 'Fernando', 'Fecha': '2020-02-18', 'Texto': 'Mi Segundo Mensaje'}]
+# Juan
+# Fernando
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
