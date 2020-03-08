f64bfa0eec7f2a5d21ca2c1b37da97e448c094ef
Juan Romero <jfromero160@hotmail.com>
2020-03-07 18:25:49 -0600

    Create plantilla.py

---
+++ plantilla.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+''' Mi script '''
+
+'''
+	@author Juan Romero
+	@version 1.0
+	@license GPL 3.0 
+	
+	Twitter : @jfromero88
+	YouTube : Hack Rock
+
+	[Disclaimer]
+		  Este script tiene la funcion de...
+	[/Disclaimer]
+'''
+
+#imports
+
+import msvcrt
+import pyfiglet
+from pyfiglet import Figlet
+
+#Inicio del codigo
+
+custom_fig = Figlet(font='banner3-D')
+print(custom_fig.renderText('HACK ROCK'))
+
+
+# Fin del Codigo  
+
+msvcrt.getch()
